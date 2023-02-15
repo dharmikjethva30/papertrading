0011dafcc64c8fe5d5bb665b05b5648d44ff0c4c
Gohil Deepak <93052295+deepakgohil9@users.noreply.github.com>
2023-02-15 13:02:16 +0530

    app.js refactored

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-// const fetch_data = require('./utils/fetch-data');
 const express = require('express');
 const dotenv = require("dotenv")
-const app = express();
 const order = require("./routes/order")
-dotenv.config()
 const PORT = process.env.PORT || 3000;
 
+dotenv.config()
+
+const app = express();
 
 const connect = () => {
     mongoose.connect(process.env.MONGO_URL)
@@ -14,15 +14,14 @@ const connect = () => {
             console.log("Connected to database");
         })
         .catch((err) => {
-            console.log("[ERROR]")
+            console.log("[!ERROR]")
             console.log(err);
             throw new Error
         })
 }
+
 app.use(express.json())
 app.use("/order", order)
-
-
 
 app.listen(PORT, () => {
     connect()
