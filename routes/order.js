50e1ba185d7577209c5b4301110c1f62407d12ba
Dharmik Jethva <97388367+dharmikjethva30@users.noreply.github.com>
2023-02-20 11:10:51 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const express = require('express');
 const order = require('../controllers/order');
 const route = express.Router();
 
-route.get("/current",order.current_order)
+route.post("/current",order.current_order)
 route.post("/buy",order.buy_order)
 route.post("/sell",order.sell_order)
 
